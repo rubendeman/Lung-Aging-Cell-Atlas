f1f77f96e93d93d2d4325e7187d0eda1b73a04f7
Ruben De Man <ruben.deman@gmail.com>
2026-04-21 13:03:51 -0400

    Update libra_bulk_hisample.R

--- GLMM Scripts/GeneAge/libra_bulk_hisample.R
+++ GLMM Scripts/GeneAge/libra_bulk_hisample.R
@@ -1,5 +1,3 @@
-#.libPaths(c("/home/rd796/project/R/4.2", .libPaths()))
-.libPaths(c("/home/rd796/project/R/4.2","/home/rd796/R/4.2", .libPaths()))
 library(Seurat)
 library(boot)
 library(Libra)
@@ -11,17 +9,16 @@ options(future.globals.maxSize = 5000 * 1024^2)
 options(future.seed=TRUE)
 
 ####### set working directory
-my.workingDir <- "/home/rd796/palmer_scratch"
+my.workingDir <- ?
 
 setwd(my.workingDir)
 
 ####### load the seurat object
-load('/home/rd796/project/ageproj/imm12_10.RData')
-DefaultAssay(immune.combined)<-'RNA'
-immune.combined$predicted.id[immune.combined$predicted.id=='AT2B']<-'AT2'
-immune.combined$predicted.id[immune.combined$predicted.id=='AT2S']<-'AT2'
+#LOAD INTEGRATED OBJECT
 
-comp<-list(Epithelial=c('AT1','AT2','Basal','Ciliated','Club','Goblet'),Endothelial=c('Lymphatic','Peribronchial','Aerocyte','gCap','Arterial','Venous'),Mesenchymal=c('Adventitial Fibroblast','Alv. Fibroblast','Myofibroblast','SMC','Pericyte'),Myeloid=c('Monocyte','Macrophage','Alv. Macrophage'),Lymphoid=c('B','T','Mast','DC','NK'))
+DefaultAssay(immune.combined)<-'RNA'
+
+comp<-list(Epithelial=c('AT1','AT2B','AT2S','Basal','Ciliated','Club','Goblet'),Endothelial=c('Lymphatic','Peribronchial','Aerocyte','gCap','Arterial','Venous'),Mesenchymal=c('Adventitial Fibroblast','Alv. Fibroblast','Myofibroblast','SMC','Pericyte'),Myeloid=c('Monocyte','Macrophage','Alv. Macrophage'),Lymphoid=c('B','T','Mast','DC','NK'))
 cell.types<-unlist(comp)
 
 immune.combined<-subset(immune.combined,subset=predicted.id %in% cell.types,invert=F)
@@ -38,4 +35,3 @@ meta<-immune.combined@meta.data
 res.t <- run_de(immune.combined, meta=meta, cell_type_col="predicted.id",label_col="agebin",replicate_col="orig.ident")
 
 ### define where the output is being written to 
-save(res.t,file='/home/rd796/palmer_scratch/libra_out_hisample.RData')
