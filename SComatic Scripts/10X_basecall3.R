c750d7882c5553c63486188dd441d06bede37d65
Ruben De Man <ruben.deman@gmail.com>
2026-04-21 13:42:11 -0400

    Update 10X_basecall3.R

--- SComatic Scripts/10X_basecall3.R
+++ SComatic Scripts/10X_basecall3.R
@@ -17,12 +17,12 @@ soup.sample.names <- list.files(file.path(sample.parent.dir))
 soup.sample.dir.paths <- file.path(sample.parent.dir, soup.sample.names)
 jobsub.filepath <- file.path(basecall.scripts.dir,"basecall3.jobsub.bat")
 
-py.path1 <- "/home/rd796/project/SComatic-main/scripts/MergeCounts/MergeBaseCellCounts.py"
-py.path2 <- "/home/rd796/project/SComatic-main/scripts/BaseCellCalling/BaseCellCalling.step1.py"
-py.path3 <- "/home/rd796/project/SComatic-main/scripts/BaseCellCalling/BaseCellCalling.step2.py"
-ref.path <- "/home/rd796/project/GRCh38.primary_assembly.genome.fa"
-editing='/home/rd796/project/SComatic-main/RNAediting/AllEditingSites.hg38.txt'
-PON='/home/rd796/project/SComatic-main/PoNs/PoN.scRNAseq.hg38.tsv'
+py.path1 <- "SComatic-main/scripts/MergeCounts/MergeBaseCellCounts.py"
+py.path2 <- "SComatic-main/scripts/BaseCellCalling/BaseCellCalling.step1.py"
+py.path3 <- "SComatic-main/scripts/BaseCellCalling/BaseCellCalling.step2.py"
+ref.path <- "GRCh38.primary_assembly.genome.fa"
+editing='SComatic-main/RNAediting/AllEditingSites.hg38.txt'
+PON='SComatic-main/PoNs/PoN.scRNAseq.hg38.tsv'
 
 ## Wipe the old jobsub.bat file
 cat("",sep="",file=jobsub.filepath, append=FALSE)
@@ -42,7 +42,7 @@ for(i in 1:length(soup.sample.names)){
                     " --outfile ", soup.sample.dir.paths[i], "/basecall2/", soup.sample.names[i],
                     " --editing ", editing,
                     " --pon ", PON, "\n",
-                    "bedtools intersect -header -a ",soup.sample.dir.paths[i],"/basecall2/",soup.sample.names[i],".calling.step2.tsv -b /home/rd796/project/SComatic-main/bed_files_of_interest/UCSC.k100_umap.without.repeatmasker.bed | awk '$1 ~ /^#/ || $6 == \"PASS\"' > ",soup.sample.dir.paths[i],"/basecall2/",soup.sample.names[i],".calling.step2.pass.tsv",
+                    "bedtools intersect -header -a ",soup.sample.dir.paths[i],"/basecall2/",soup.sample.names[i],".calling.step2.tsv -b SComatic-main/bed_files_of_interest/UCSC.k100_umap.without.repeatmasker.bed | awk '$1 ~ /^#/ || $6 == \"PASS\"' > ",soup.sample.dir.paths[i],"/basecall2/",soup.sample.names[i],".calling.step2.pass.tsv",
                     sep="")
 cat(cmd.out,file=script.filepath,append=F)
 cat("sbatch ",script.filepath,"\n",sep="",file=jobsub.filepath, append=TRUE)
